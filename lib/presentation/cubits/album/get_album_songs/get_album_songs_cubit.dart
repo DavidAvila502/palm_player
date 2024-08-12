1f8567480aed5f678c5377a510046573d52a7111
David Avila <davidavila6002@gmail.com>
2024-08-11 18:58:37 -0600

    album cubits 3/3 done.

---
+++ lib/presentation/cubits/album/get_album_songs/get_album_songs_cubit.dart
@@ -0,0 +1,25 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:palm_player/domain/entities/song.dart';
+import 'package:palm_player/domain/use_cases/album_use_cases.dart';
+import 'package:palm_player/presentation/cubits/album/get_album_songs/get_album_songs_state.dart';
+
+class GetAlbumSongsCubit extends Cubit<GetAlbumSongsState> {
+  final AlbumUseCases _albumUseCases;
+
+  GetAlbumSongsCubit(this._albumUseCases)
+      : super(const GetAlbumSongsStateInitial());
+
+  Future<void> getAlbumSongs(int id) async {
+    if (state is! GetAlbumSongsStateLoaded) {
+      try {
+        emit(const GetAlbumSongsStateLoading());
+
+        List<Song> response = await _albumUseCases.getAlbumSongs(id);
+
+        emit(GetAlbumSongsStateLoaded(response));
+      } catch (_) {
+        emit(const GetAlbumSongsStateError('Error trying to load the songs.'));
+      }
+    }
+  }
+}
