0e9d78e386b3a506f1403702823c4d7a9e6b2a7c
David Avila <davidavila6002@gmail.com>
2024-09-23 20:40:26 -0600

    added PopScope to BottomNavigaor

--- lib/presentation/navigators/bottom_navigator.dart
+++ lib/presentation/navigators/bottom_navigator.dart
@@ -24,8 +24,10 @@ class _BottomNavigator extends State<BottomNavigator> {
     SelectedAlbumScreen()
   ];
   int _selectedIndex = 0;
+  int _lastSelectedIndex = 0;
   bool _isExpandibleSmall = true;
   bool showHiddenScreen = false;
+  bool isPopScreen = false;
 
   void setIsExpandibleSmall(bool param) {
     setState(() {
@@ -35,8 +37,27 @@ class _BottomNavigator extends State<BottomNavigator> {
 
   void setSelectedScreen(int index) {
     setState(() {
+      isPopScreen = false;
+      _lastSelectedIndex = _selectedIndex;
       _selectedIndex = index;
     });
+  }
+
+  void _onWillPop() {
+    if (_selectedIndex == 0) {
+      return;
+    }
+
+    if (isPopScreen) {
+      setSelectedScreen(0);
+      return;
+    }
+
+    setSelectedScreen(_lastSelectedIndex);
+    setState(() {
+      isPopScreen = true;
+    });
+    return;
   }
 
   @override
@@ -45,72 +66,78 @@ class _BottomNavigator extends State<BottomNavigator> {
     double bottomNavigationBarHeight = kBottomNavigationBarHeight;
     // final visibleScreens = showHiddenScreen ? _screens : _screens.sublist(0, 3);
 
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      backgroundColor: const Color.fromRGBO(26, 27, 32, 1),
-      body: SafeArea(
-          child: RepositoryProvider<HandleBottomNavigationIndex>(
-        create: (context) =>
-            HandleBottomNavigationIndex(setSelectedScreen: setSelectedScreen),
-        child: Stack(children: [
-          IndexedStack(index: _selectedIndex, children: _screens),
-          Positioned(
-              left: 0,
-              right: 0,
-              bottom: 0,
-              child: SizedBox(
-                  height: screenHeight * 1,
-                  child: ExpandiblePlayerController(
-                    notifyToBottomNavigatorExpandibleIsSmall:
-                        setIsExpandibleSmall,
-                  )))
-        ]),
-      )),
-      bottomNavigationBar: AnimatedContainer(
-        duration: const Duration(milliseconds: 400),
-        curve: Curves.easeInOut,
-        decoration: BoxDecoration(
-          color: _isExpandibleSmall
-              ? Theme.of(context).primaryColor
-              : const Color.fromRGBO(26, 27, 32, 1),
+    return PopScope(
+      canPop: _selectedIndex == 0 ? true : false,
+      onPopInvoked: (bool didPop) {
+        _onWillPop();
+      },
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        backgroundColor: const Color.fromRGBO(26, 27, 32, 1),
+        body: SafeArea(
+            child: RepositoryProvider<HandleBottomNavigationIndex>(
+          create: (context) =>
+              HandleBottomNavigationIndex(setSelectedScreen: setSelectedScreen),
+          child: Stack(children: [
+            IndexedStack(index: _selectedIndex, children: _screens),
+            Positioned(
+                left: 0,
+                right: 0,
+                bottom: 0,
+                child: SizedBox(
+                    height: screenHeight * 1,
+                    child: ExpandiblePlayerController(
+                      notifyToBottomNavigatorExpandibleIsSmall:
+                          setIsExpandibleSmall,
+                    )))
+          ]),
+        )),
+        bottomNavigationBar: AnimatedContainer(
+          duration: const Duration(milliseconds: 400),
+          curve: Curves.easeInOut,
+          decoration: BoxDecoration(
+            color: _isExpandibleSmall
+                ? Theme.of(context).primaryColor
+                : const Color.fromRGBO(26, 27, 32, 1),
+          ),
+          child: AnimatedSwitcher(
+              duration: const Duration(milliseconds: 200),
+              transitionBuilder: (Widget child, Animation<double> animation) {
+                return SlideTransition(
+                  position: Tween<Offset>(
+                    begin: const Offset(0, 1),
+                    end: Offset.zero,
+                  ).animate(animation),
+                  child: child,
+                );
+              },
+              child: _isExpandibleSmall
+                  ? ClipRRect(
+                      key: const ValueKey('BottomNavigationBar'),
+                      borderRadius: const BorderRadius.only(
+                          topLeft: Radius.circular(30),
+                          topRight: Radius.circular(30)),
+                      child: CustomBottomNavigationBar(
+                        selectedItemColor: Theme.of(context).primaryColor,
+                        selectedIndex: _selectedIndex,
+                        backgroundColor: Colors.black,
+                        items: const <CustomBottomNavigationBarItem>[
+                          CustomBottomNavigationBarItem(
+                              icon: Icons.home, label: 'Home'),
+                          CustomBottomNavigationBarItem(
+                              icon: Icons.search, label: 'Search'),
+                          CustomBottomNavigationBarItem(
+                              icon: Icons.settings, label: 'Settings')
+                        ],
+                        onTap: (int index) {
+                          setSelectedScreen(index);
+                        },
+                      ),
+                    )
+                  : Container(
+                      key: const ValueKey('HideContainer'),
+                      height: bottomNavigationBarHeight)),
         ),
-        child: AnimatedSwitcher(
-            duration: const Duration(milliseconds: 200),
-            transitionBuilder: (Widget child, Animation<double> animation) {
-              return SlideTransition(
-                position: Tween<Offset>(
-                  begin: const Offset(0, 1),
-                  end: Offset.zero,
-                ).animate(animation),
-                child: child,
-              );
-            },
-            child: _isExpandibleSmall
-                ? ClipRRect(
-                    key: const ValueKey('BottomNavigationBar'),
-                    borderRadius: const BorderRadius.only(
-                        topLeft: Radius.circular(30),
-                        topRight: Radius.circular(30)),
-                    child: CustomBottomNavigationBar(
-                      selectedItemColor: Theme.of(context).primaryColor,
-                      selectedIndex: _selectedIndex,
-                      backgroundColor: Colors.black,
-                      items: const <CustomBottomNavigationBarItem>[
-                        CustomBottomNavigationBarItem(
-                            icon: Icons.home, label: 'Home'),
-                        CustomBottomNavigationBarItem(
-                            icon: Icons.search, label: 'Search'),
-                        CustomBottomNavigationBarItem(
-                            icon: Icons.settings, label: 'Settings')
-                      ],
-                      onTap: (int index) {
-                        setSelectedScreen(index);
-                      },
-                    ),
-                  )
-                : Container(
-                    key: const ValueKey('HideContainer'),
-                    height: bottomNavigationBarHeight)),
       ),
     );
   }
